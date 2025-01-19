0cac334a13edfd4912c31a110d4969aa17302ad0
kisunghoon <kisunghoon29@gmail.com>
2025-01-19 10:23:07 +0900

    프로젝트 설정 커밋

---
+++ src/test/java/com/zerobase/mentalgrowhdiary/MentalGrowhDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.mentalgrowhdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalGrowhDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
